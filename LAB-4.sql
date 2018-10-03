6743e53085fff23be4c6776b35dbdb4f5e579938
Almajesty15 <Almaz.Oral2000@gmail.com>
2018-10-03 13:40:38 +0600

    ll

--- LAB-4.sql
+++ LAB-4.sql
@@ -1,31 +1,55 @@
-CREATE DATABASE LAB4;
+CREATE DATABASE lab5;
+
 CREATE TABLE customers(
-  id SERIAL primary key ,
-  name varchar(255),
-  city varchar(255),
-  grade INT,
-  salesman_id int
+  customer_id integer,
+  cust_name varchar(255) NOT NULL ,
+  city varchar(255) NOT NULL,
+  grade integer,
+  salesman_id integer
 );
-drop table customers;
-INSERT INTO customers (id,name, city, grade, salesman_id) VALUES (3002, 'Nick Rimando', 'New York', 100,5001),
-                             (3005,'Graham Zusi','California',200,5002),
-                             (3001,'Brad Guzan','London',5 ,5005);
-SELECT * FROM customers WHERE grade>100;
-SELECT * FROM customers WHERE city='New York' AND grade>100;
-SELECT * FROM customers WHERE city='New York' OR grade>100;
-SELECT * FROM customers WHERE city='Paris' OR city='Roma';
-SELECT * FROM customers WHERE name LIKE 'B%';
+SELECT * FROM customers;
+
+INSERT INTO customers(customer_id, cust_name, city, grade, salesman_id) VALUES (3002,'Nick Rimando','New York',100,5001);
+INSERT INTO customers(customer_id, cust_name, city, grade, salesman_id) VALUES (3005,'Graham Zusi','California',200,5002);
+INSERT INTO customers(customer_id, cust_name, city, salesman_id) VALUES (3001,'Brad Guzan','London',5005);
+INSERT INTO customers(customer_id, cust_name, city, grade, salesman_id) VALUES (3004,'Fabian Johns','Paris',300,5006);
+INSERT INTO customers(customer_id, cust_name, city, grade, salesman_id) VALUES (3007,'Brad Davis','New York',200,5001);
+INSERT INTO customers(customer_id, cust_name, city, grade, salesman_id) VALUES (3009,'Geoff Camero','Berlin',100,5003);
+INSERT INTO customers(customer_id, cust_name, city, grade, salesman_id) VALUES (3008,'Julian Green','London',300,5002);
+
 CREATE TABLE orders(
-  order_id SERIAL PRIMARY KEY ,
-  customer_id INT,
-  description TEXT,
-  purch_amt INT
+  ord_no serial PRIMARY KEY,
+  purch_amt FLOAT,
+  ord_date date,
+  customer_id INTEGER,
+  salesman_id INTEGER
 );
-SELECT * FROM orders WHERE customer_id IN( SELECT id FROM customers WHERE city='New York');
-SELECT * FROM customers WHERE ID IN(SELECT customer_id FROM orders GROUP BY customer_id HAVING COUNT(customer_id)>10);
-SELECT sum(purch_amt) FROM orders;
-SELECT AVG(purch_amt) FROM orders;
-SELECT * FROM customers WHERE name IS NOT NULL;
-SELECT MAX(purch_amt) FROM ORDERS;
+
+CREATE TABLE sales(
+  salesman_id integer,
+  name varchar(255) not null ,
+  city varchar(255) not null,
+  commission float
+);
+
 DROP TABLE orders;
-DROP TABLE customers;
+
+SELECT Sum(purch_amt) FROM orders;
+
+SELECT AVG (purch_amt) FROM orders;
+
+SELECT count(*) FROM customers WHERE cust_name IS NOT NULL;
+
+SELECT min(purch_amt) FROM orders;
+
+SELECT * FROM customers WHERE  cust_name LIKE '%b';
+
+SELECT * FROM orders WHERE customer_id = (SELECT customer_id FROM customers WHERE city = 'New York');
+
+SELECT * FROM customers WHERE customer_id = (SELECT customer_id FROM orders WHERE purch_amt>=10);
+
+SELECT sum(grade) FROM customers;
+
+SELECT * FROM customers WHERE cust_name IS NOT NULL ;
+
+SELECT max(grade) FROM customers;
